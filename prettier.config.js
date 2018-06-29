34c18c96f206d6c71918b34926a82a58e4f58b3e
Alexander Kirkhgezner <alex1kirch@gmail.com>
2018-06-30 04:59:20 +0500

    Add prettier config

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    printWidth: 100,
+    semi: true,
+    tabWidth: 4,
+    singleQuote: false,
+    trailingComma: "all",
+};
