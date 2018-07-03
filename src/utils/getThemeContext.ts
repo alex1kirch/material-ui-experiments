2725cb6c7964b6aedfbb6bbb7f4025f79f43ba57
Alexander Kirkhgezner <alex1kirch@users.noreply.github.com>
2018-07-04 01:41:56 +0500

    Fix style prefix in react-jss

--- src/utils/getThemeContext.ts
+++ src/utils/getThemeContext.ts
@@ -24,7 +24,7 @@ function createThemeContext() {
 
     return {
         generateClassName: createGenerateClassName({
-            productionPrefix: "jtest",
+            productionPrefix: "mui-exp",
         }),
         jss,
         muiTheme,
